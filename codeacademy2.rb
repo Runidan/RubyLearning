c3b3ab1733957e5d8a571132a0416524c32b4fec
Runidan <45917212+Runidan@users.noreply.github.com>
2022-03-12 15:59:46 +0500

    add codeacadeemy2

---
+++ codeacademy2.rb
@@ -0,0 +1,19 @@
+print "What is your name? "
+first_name = gets.chomp
+fistr_name.capitalize!
+puts "Your first name is #{first_name}"
+
+print "what is your last name? "
+last_name = gets.chomp
+last_name.capitalize!
+puts "Your last name is #{last_name}"
+
+print "Where are you city from? "
+city = gets.chomp
+city.capitalize!
+puts "Your city is #{city}"
+
+print "Which state?"
+state  = gets.chomp
+state.upcase!
+puts "Your state is #{state}"
