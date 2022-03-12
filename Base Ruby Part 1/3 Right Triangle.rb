f2065c2cd62f54c6ab5e767a7c10cd1dc0c30680
Runidan <45917212+Runidan@users.noreply.github.com>
2022-03-12 21:59:05 +0500

    Create 3 Right Triangle.rb

---
+++ Base Ruby Part 1/3 Right Triangle.rb
@@ -0,0 +1,23 @@
+=begin
+Программа запрашивает у пользователя 3 стороны треугольника и определяет, является ли треугольник прямоугольным,
+равнобедренным  или равносторонним и выводит результат на экран.
+=end
+
+print "Введите длину стороны 1: "
+hypotenuse = gets.chomp.to_i
+print "Введите длину стороны 2: "
+leg1 = gets.chomp.to_i
+hypotenuse, leg1 = leg1, hypotenuse if leg1 > hypotenuse
+print "Введите длину стороны 3: "
+leg2 = gets.chomp.to_i
+hypotenuse, leg2 = leg2, hypotenuse if leg2 > hypotenuse
+
+if hypotenuse**2 == leg1**2 + leg2**2
+  puts "Треугольник является прямоугольным"
+elsif hypotenuse == leg1 && hypotenuse == leg2
+  puts "Треугольник является равносторонним"
+elsif hypotenuse == leg1 || hypotenuse == leg2
+  puts "Треугольник является равнобедренным"
+else
+  puts "Треугольник не является ни прямоугольным, ни равносторонним, ни равнобедренным"
+end
